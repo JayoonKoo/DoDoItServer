0b26d76f2c11723922325101b05ef7e1df2b3c74
jayoonkoo <rnwkdbs12@gmail.com>
2023-10-11 21:00:09 +0900

    feat: cookieparser, 보안 관련 패키지 추가

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+
 import env from './config/env';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(cookieParser(env.cookieSecret));
+
 app.listen(env.port, () => {
-  console.log('서버 실행');
+  console.log(`${env.port} 포트에서 서버 실행중`);
 });
--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 const env = {
   port: process.env.PORT,
+  cookieSecret: process.env.COOKIE_SECRET,
 };
 
 export default env;
